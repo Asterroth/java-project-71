0ca05debc41b921fbaab91daf045f99fb5c956b4
Asterroth <asterroth@yandex.ru>
2024-01-20 17:21:36 +0300

    add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,21 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+@CommandLine.Command(name = "getDiff", mixinStandardHelpOptions = true, version = "getDiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
 public class App {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean usageHelpRequested;
+    @Option(names = {"-V", "--version"}, versionHelp = true,
+            description = "Print version information and exit.")
+    private boolean versionInfoRequested;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
