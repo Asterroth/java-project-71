c2de021e61f34e3585cf99cfb97782d35f537ee0
Asterroth <asterroth@yandex.ru>
2024-01-20 20:36:05 +0300

    add options and params to picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,11 +14,35 @@ import picocli.CommandLine.Parameters;
 
 public class App implements Runnable {
 
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    @Option(names = {"-h", "--help"},
+            usageHelp = true,
+            description = "Show this help message and exit."
+    )
     private boolean usageHelp;
-    @Option(names = {"-V", "--version"}, versionHelp = true,
-            description = "Print version information and exit.")
+
+    @Option(names = {"-V", "--version"},
+            versionHelp = true,
+            description = "Print version information and exit."
+    )
     private boolean versionInfo;
+
+    @Option(names = {"-f", "--format"},
+            paramLabel = "format",
+            defaultValue = "stylish",
+            description = "output format [default: stylish]"
+    )
+    private String format;
+
+    @Parameters(index = "0",
+            paramLabel = "filepath1",
+            description = "path to first file"
+    )
+    private String pathToFile1;
+
+    @Parameters(index = "1",
+            paramLabel = "filepath2",
+            description = "path to second file")
+    private String pathToFile2;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
