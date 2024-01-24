db7e04f1791289640e500d4fecb7581366f1f964
Asterroth <anton.malukhin@gmail.com>
2024-01-24 13:52:04 +0300

    test coverage

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -13,6 +13,9 @@ import java.util.TreeSet;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ *
+ */
 public class Differ {
     public static String generate(String pathToFile1, String pathToFile2) throws IOException {
         List<String> resultList = new ArrayList<>();
@@ -25,8 +28,8 @@ public class Differ {
         File file2 = new File(String.valueOf(path2));
 
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> jsonData1 = mapper.readValue(file1, new TypeReference<>(){});
-        Map<String, Object> jsonData2 = mapper.readValue(file2, new TypeReference<>(){});
+        Map<String, Object> jsonData1 = mapper.readValue(file1, new TypeReference<>() { });
+        Map<String, Object> jsonData2 = mapper.readValue(file2, new TypeReference<>() { });
 
         SortedSet<String> totalKeys = new TreeSet<>(jsonData1.keySet());
         totalKeys.addAll(jsonData2.keySet());
