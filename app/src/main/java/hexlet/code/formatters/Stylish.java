587c428d0091453e5dd6b80b6cb2db9a559816e4
Asterroth <anton.malukhin@gmail.com>
2024-02-06 22:31:11 +0300

    Update tests

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -10,14 +10,14 @@ public class Stylish {
             Map<String, Object> map = diffData.get(key);
 
             if (map.get("status").equals("added")) {
-                result.append(" + ").append(key).append(": ").append(map.get("value2")).append("\n");
+                result.append("  + ").append(key).append(": ").append(map.get("value2")).append("\n");
             } else if (map.get("status").equals("deleted")) {
-                result.append(" - ").append(key).append(": ").append(map.get("value1")).append("\n");
+                result.append("  - ").append(key).append(": ").append(map.get("value1")).append("\n");
             } else if (map.get("status").equals("unchanged")) {
-                result.append("   ").append(key).append(": ").append(map.get("value1")).append("\n");
+                result.append("    ").append(key).append(": ").append(map.get("value1")).append("\n");
             } else {
-                result.append(" - ").append(key).append(": ").append(map.get("value1")).append("\n");
-                result.append(" + ").append(key).append(": ").append(map.get("value2")).append("\n");
+                result.append("  - ").append(key).append(": ").append(map.get("value1")).append("\n");
+                result.append("  + ").append(key).append(": ").append(map.get("value2")).append("\n");
             }
         }
 
--- app/src/test/java/TestDiffer.java
+++ app/src/test/java/TestDiffer.java
@@ -1,100 +1,46 @@
-import hexlet.code.Differ;
-import org.junit.jupiter.api.Test;
 
+import hexlet.code.Differ;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class TestDiffer {
+class TestDiffer {
+    private static String expectedJson;
+    private static String expectedPlain;
+    private static String expectedStylish;
 
     private static Path getFixturePath(String fileName) {
         return Paths.get("src", "test", "resources", "fixtures", fileName)
                 .toAbsolutePath().normalize();
     }
-    @Test
-    void differTestJson() throws Exception {
-        String resultJson = "{\n"
-                + " - follow: false\n"
-                + "   host: hexlet.io\n"
-                + " - proxy: 123.234.53.22\n"
-                + " - timeout: 50\n"
-                + " + timeout: 20\n"
-                + " + verbose: true\n"
-                + "}";
-        String path1 = getFixturePath("file1.json").toString();
-        String path2 = getFixturePath("file2.json").toString();
-        assertEquals(resultJson, Differ.generate(path1, path2));
+
+    private static String readFixture(String fileName) throws Exception {
+        Path filePath = getFixturePath(fileName);
+        return Files.readString(filePath).trim();
     }
 
-    @Test
-    void differTestYaml() throws Exception {
-        String resultJson = "{\n"
-                + " - follow: false\n"
-                + "   host: hexlet.io\n"
-                + " - proxy: 123.234.53.22\n"
-                + " - timeout: 50\n"
-                + " + timeout: 20\n"
-                + " + verbose: true\n"
-                + "}";
-        String path1 = getFixturePath("file1.yml").toString();
-        String path2 = getFixturePath("file2.yml").toString();
-        assertEquals(resultJson, Differ.generate(path1, path2));
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        expectedJson = readFixture("expected-json");
+        expectedPlain = readFixture("expected-plain");
+        expectedStylish = readFixture("expected-stylish");
     }
 
-    @Test
-    void differTestJson2() throws Exception {
-        String resultJson = "{\n"
-                + "   chars1: [a, b, c]\n"
-                + " - chars2: [d, e, f]\n"
-                + " + chars2: false\n"
-                + " - checked: false\n"
-                + " + checked: true\n"
-                + " - default: null\n"
-                + " + default: [value1, value2]\n"
-                + " - id: 45\n"
-                + " + id: null\n"
-                + " - key1: value1\n"
-                + " + key2: value2\n"
-                + "   numbers1: [1, 2, 3, 4]\n"
-                + " - numbers2: [2, 3, 4, 5]\n"
-                + " + numbers2: [22, 33, 44, 55]\n"
-                + " - numbers3: [3, 4, 5]\n"
-                + " + numbers4: [4, 5, 6]\n"
-                + " + obj1: {nestedKey=value, isNested=true}\n"
-                + " - setting1: Some value\n"
-                + " + setting1: Another value\n"
-                + " - setting2: 200\n"
-                + " + setting2: 300\n"
-                + " - setting3: true\n"
-                + " + setting3: none\n"
-                + "}";
-        String path1 = getFixturePath("file3.json").toString();
-        String path2 = getFixturePath("file4.json").toString();
-        assertEquals(resultJson, Differ.generate(path1, path2));
-    }
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void generateTest(String format) throws Exception {
+        String path1 = getFixturePath("file1." + format).toString();
+        String path2 = getFixturePath("file2." + format).toString();
 
-    @Test
-    void differTestPlainText() throws Exception {
-        String resultPlainText = "Property 'chars2' was updated. From [complex value] to false\n" +
-                "Property 'checked' was updated. From false to true\n" +
-                "Property 'default' was updated. From null to [complex value]\n" +
-                "Property 'id' was updated. From 45 to null\n" +
-                "Property 'key1' was removed\n" +
-                "Property 'key2' was added with value: 'value2'\n" +
-                "Property 'numbers2' was updated. From [complex value] to [complex value]\n" +
-                "Property 'numbers3' was removed\n" +
-                "Property 'numbers4' was added with value: [complex value]\n" +
-                "Property 'obj1' was added with value: [complex value]\n" +
-                "Property 'setting1' was updated. From 'Some value' to 'Another value'\n" +
-                "Property 'setting2' was updated. From 200 to 300\n" +
-                "Property 'setting3' was updated. From true to 'none'";
-        String path1 = getFixturePath("file3.json").toString();
-        String path2 = getFixturePath("file4.json").toString();
-        assertEquals(resultPlainText, Differ.generate(path1, path2, "plain"));
-    }
-
-    @Test
-    void differJson() throws Exception {
-
+        assertEquals(expectedStylish, Differ.generate(path1, path2));
+        assertEquals(expectedStylish, Differ.generate(path1, path2, "stylish"));
+        assertEquals(expectedPlain, Differ.generate(path1, path2, "plain"));
+        assertEquals(expectedJson, Differ.generate(path1, path2, "json"));
     }
 }
